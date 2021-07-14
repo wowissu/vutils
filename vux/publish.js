572075fef340c9c47234a9714d0113e1f4d74999
wowissu <bbgbbg0123@gmail.com>
2021-07-14 14:28:25 +0800

    feat(vux): publish add ask part

--- vux/publish.js
+++ vux/publish.js
@@ -1,29 +1,54 @@
 #!/usr/bin/env node
-const yargs = require('yargs/yargs')
+const yargs = require('yargs/yargs');
+const chalk = require('chalk');
 const { hideBin } = require('yargs/helpers')
 const { execSync } = require('child_process');
 
 // --patch ... --prerelease
-let v = ""
+let action = ""
 
 const argv = yargs(hideBin(process.argv))
-  .option('major', { desc: '1.0.0 => 1.0.1', coerce(val) { v = val ? '--major' : v; return val; } })
-  .option('minor', { desc: '1.0.0 => 1.1.0', coerce(val) { v = val ? '--minor' : v; return val; } })
-  .option('patch', { desc: '1.0.0 => 1.0.1', coerce(val) { v = val ? '--patch' : v; return val; } })
-  .option('premajor', { desc: '1.0.0-0 => 1.0.1-0', coerce(val) { v = val ? '--premajor' : v; return val; } })
-  .option('preminor', { desc: '1.0.0-0 => 1.1.0-0', coerce(val) { v = val ? '--preminor' : v; return val; } })
-  .option('prepatch', { desc: '1.0.0-0 => 1.0.1-0', coerce(val) { v = val ? '--prepatch' : v; return val; } })
-  .option('prerelease', { desc: '1.0.0-0 => 1.0.0-1', coerce(val) { v = val ? '--prerelease' : v; return val; } })
+  .option('major', { desc: '1.0.0 => 1.0.1', coerce(val) { action = val ? '--major' : action; return val; } })
+  .option('minor', { desc: '1.0.0 => 1.1.0', coerce(val) { action = val ? '--minor' : action; return val; } })
+  .option('patch', { desc: '1.0.0 => 1.0.1', coerce(val) { action = val ? '--patch' : action; return val; } })
+  .option('premajor', { desc: '1.0.0-0 => 1.0.1-0', coerce(val) { action = val ? '--premajor' : action; return val; } })
+  .option('preminor', { desc: '1.0.0-0 => 1.1.0-0', coerce(val) { action = val ? '--preminor' : action; return val; } })
+  .option('prepatch', { desc: '1.0.0-0 => 1.0.1-0', coerce(val) { action = val ? '--prepatch' : action; return val; } })
+  .option('prerelease', { desc: '1.0.0-0 => 1.0.0-1', coerce(val) { action = val ? '--prerelease' : action; return val; } })
   .argv
 
-const stdout = execSync(`yarn publish --access public ${v} --no-git-tag-version --no-commit-hooks`);
+const stdout = execSync(`yarn version ${action} --no-git-tag-version --no-commit-hooks`);
 const pkg = require('./package.json');
 const tagLabel = `${pkg.name}-v${pkg.version}`;
 
 console.log(`stdout: ${stdout}`);
-console.log(`tagLabel: ${tagLabel}`);
 
-execSync(`git add .`);
-execSync(`git ci -m "${tagLabel}"`);
-execSync(`git tag -a "${tagLabel}" -m "${tagLabel}"`);
 
+process.stdin.setEncoding('utf-8');
+
+process.stdout.write(chalk`\n{bold.yellow Confirm！} 再次輸入版本號碼 {bold.green ${pkg.version}} 以繼續動作 \n[輸入{bold.green ${pkg.version}}]:`);
+
+process.stdin.on('readable', () => {
+  const input = process.stdin.read().trim();
+
+  if (input !== null) {
+    if (input === pkg.version) {
+      const stdout = execSync(`yarn publish --access public --no-git-tag-version --no-commit-hooks --new-version ${pkg.version}`);
+
+      console.log(stdout);
+
+    } else {
+      process.exit(1);
+    }
+  }
+});
+
+function gitHandler() {
+
+
+  execSync(`git add .`);
+  execSync(`git ci -m "${tagLabel}"`);
+  execSync(`git tag -a "${tagLabel}" -m "${tagLabel}"`);
+
+  console.log(`tagLabel: ${tagLabel}`);
+}
